5f558b42c2fcfc0f4354f58732d4ddf3fde04b14
Mohit Pandey <mohitpandey825@gmail.com>
2025-01-25 17:46:50 +0530

    splitting other components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import AccordionContainer from "./components/Accordion/AccordionContainer";
 import Header from "./components/Header";
-import ImageSliderContainer from "./components/image-slider/ImageSliderContainer";
 import LiveStreaming from "./components/Live Streaming/Live Streaming";
 import CommentsContainer from "./components/nested-comments/commentsContainer";
 import SearchBar from "./components/SearchBar/SearchBar";
 import ProtectedRoute from "./hoc/ProtectedRoute";
 import Admin from "./Links/Admin";
-import BookingContainer from "./Links/BookingContainer";
 import { Home } from "./Links/Home";
 import Login from "./Links/Login";
 import { lazy, Suspense } from "react";
@@ -17,21 +14,70 @@ const PaginationContainer = lazy(() =>
   import("./components/Pagination/PaginationContainer")
 );
 
+const ImageSliderContainer = lazy(() =>
+  import("./components/image-slider/ImageSliderContainer")
+);
+
+const BookingContainer = lazy(() => import("./Links/BookingContainer"));
+
+const AccordionContainer = lazy(() =>
+  import("./components/Accordion/AccordionContainer")
+);
+
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
         <Route path="/react-components" element={<Home />} />
-        <Route path="/booking" element={<BookingContainer />} />
+        <Route
+          path="/booking"
+          element={
+            <Suspense
+              fallback={
+                <div>
+                  <h1>Loading...</h1>
+                </div>
+              }
+            >
+              <BookingContainer />
+            </Suspense>
+          }
+        />
         <Route element={<ProtectedRoute />}>
           <Route path="/admin" element={<Admin />} />
         </Route>
 
         <Route path="/login" element={<Login />} />
-        <Route path="/accordion" element={<AccordionContainer />} />
+        <Route
+          path="/accordion"
+          element={
+            <Suspense
+              fallback={
+                <div>
+                  <h1>Loading...</h1>
+                </div>
+              }
+            >
+              <AccordionContainer />
+            </Suspense>
+          }
+        />
         <Route path="/comments" element={<CommentsContainer />} />
-        <Route path="/image-slider" element={<ImageSliderContainer />} />
+        <Route
+          path="/image-slider"
+          element={
+            <Suspense
+              fallback={
+                <div>
+                  <h1>Loading...</h1>
+                </div>
+              }
+            >
+              <ImageSliderContainer />
+            </Suspense>
+          }
+        />
         <Route path="/stream" element={<LiveStreaming />} />
         <Route path="/search" element={<SearchBar />} />
         <Route
