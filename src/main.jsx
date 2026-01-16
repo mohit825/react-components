f9c1aa3206c17c48f7249ba8b4eb90bfa5304985
mohit825 <mohitpandey825@gmail.com>
2026-01-16 12:29:01 +0530

    added continous development step"

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,8 @@
-import { scan } from "react-scan";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-scan({
-  enabled: true,
-});
 createRoot(document.getElementById("rot")).render(
   <StrictMode>
     <App />
